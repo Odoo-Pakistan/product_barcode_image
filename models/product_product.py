c594bcb706e44c0f69ffd546c6876cc97aa12f4c
Timo Talvitie <timo.talvitie@vizucom.com>
2017-03-30 12:50:45 +0300

    Add BMP

--- models/product_product.py
+++ models/product_product.py
@@ -21,12 +21,17 @@ class ProductProduct(models.Model):
 
             barcode_jpg = createBarcodeDrawing('EAN13', value=str(self.ean13), format='jpg', width=float(width), height=float(height))
             bc_data_jpg = barcode_jpg.asString('jpg')
-            self.barcode_image = base64.encodestring(bc_data_jpg)
+            self.barcode_image_jpg = base64.encodestring(bc_data_jpg)
 
             barcode_gif = createBarcodeDrawing('EAN13', value=str(self.ean13), format='gif', width=float(width), height=float(height))
             bc_data_gif = barcode_gif.asString('gif')
-            self.barcode_image = base64.encodestring(bc_data_gif)
+            self.barcode_image_gif = base64.encodestring(bc_data_gif)
+
+            barcode_bmp = createBarcodeDrawing('EAN13', value=str(self.ean13), format='bmp', width=float(width), height=float(height))
+            bc_data_bmp = barcode_bmp.asString('bmp')
+            self.barcode_image_bmp = base64.encodestring(bc_data_bmp)
 
     barcode_image = fields.Binary(compute=_get_barcode_image, string='Barcode Image', store=False)
     barcode_image_jpg = fields.Binary(compute=_get_barcode_image, string='Barcode Image (JPG)', store=False)
     barcode_image_gif = fields.Binary(compute=_get_barcode_image, string='Barcode Image (GIF)', store=False)
+    barcode_image_bmp = fields.Binary(compute=_get_barcode_image, string='Barcode Image (BMP)', store=False)
