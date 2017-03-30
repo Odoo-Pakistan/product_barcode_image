00069db3c4c4ce842cf503f38c1407c636ae6f06
Timo Talvitie <timo.talvitie@vizucom.com>
2017-03-30 12:37:30 +0300

    Test different file formats in separate fields

--- models/product_product.py
+++ models/product_product.py
@@ -19,4 +19,14 @@ class ProductProduct(models.Model):
             bc_data = barcode.asString('png')
             self.barcode_image = base64.encodestring(bc_data)
 
+            barcode_jpg = createBarcodeDrawing('EAN13', value=str(self.ean13), format='jpg', width=float(width), height=float(height))
+            bc_data_jpg = barcode_jpg.asString('jpg')
+            self.barcode_image = base64.encodestring(bc_data_jpg)
+
+            barcode_gif = createBarcodeDrawing('EAN13', value=str(self.ean13), format='gif', width=float(width), height=float(height))
+            bc_data_gif = barcode_gif.asString('gif')
+            self.barcode_image = base64.encodestring(bc_data_gif)
+
     barcode_image = fields.Binary(compute=_get_barcode_image, string='Barcode Image', store=False)
+    barcode_image_jpg = fields.Binary(compute=_get_barcode_image, string='Barcode Image (JPG)', store=False)
+    barcode_image_gif = fields.Binary(compute=_get_barcode_image, string='Barcode Image (GIF)', store=False)
